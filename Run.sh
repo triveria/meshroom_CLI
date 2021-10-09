c23e5133682a64ec8eef0f08a010c02aa8093856
AmosChenZixuan <czxvvv@foxmail.com>
2021-10-09 17:55:21 +0800

    Update Run.sh

--- Run.sh
+++ Run.sh
@@ -4,7 +4,7 @@ if [ $# -eq 0 ]
     echo "No arguments supplied"
   else
     echo "Executing Project: $1"
-    python vid2frames.py "/home/amos/workspace/DB/reconstruct/$1.mp4" "/home/amos/workspace/DB/reconstruct/$1" -s 6 -r 0.5
-    python Meshroom_CLI-linux.py "/home/amos/Programs/Meshroom-2021.1.0-av2.4.0-centos7-cuda10.2/aliceVision/bin" "/home/amos/Programs/Meshroom-2021.1.0-av2.4.0-centos7-cuda10.2/$1" "/home/amos/workspace/DB/reconstruct/$1"
+    python vid2frames.py "/home/amos/workspace/DB/reconstruct/$1.mp4" "/home/amos/Programs/Meshroom-2021.1.0/caches/$1/0_Images" -s 3 -r 0.5
+    python Meshroom_CLI-linux.py "/home/amos/Programs/Meshroom-2021.1.0/aliceVision/bin" "/home/amos/Programs/Meshroom-2021.1.0/caches/$1" "/home/amos/Programs/Meshroom-2021.1.0/caches/$1/0_Images"
 fi
 
